46f82eee23f1a3d7fa17e0e3bae4d10b30901eb2
Zuko <tansautn@users.noreply.github.com>
2025-12-15 00:14:48 +0700

    Fix syntax, wrong block statements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Hypeddit, PumpYourSound DownloadWallBypasser 2025
+// @name         Hypeddit, PumpYourSound DownloadWallBypasser 2026
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      2.1
 // @description  Bypass the fangates. Soundcloud and Spotify accounts are mandatory! Please make sure to log them on first before running the script!
 // @author       fan1200, Zuko <tansautn@gmail.com>
 // @match        https://hypeddit.com/*
@@ -283,6 +283,7 @@
 
   window.handleFollowOptions = function(containerElementId, skipperId) {
     if(document.getElementById(containerElementId) !== null) {
+      console.log(`finding ${skipperId} in #${containerElementId}`);
       document
       .getElementById(containerElementId)
       .querySelectorAll("a")
@@ -292,207 +293,156 @@
       });
 
       document.getElementById(skipperId).click();
-
-
-      window.handleSoundCloud = function() {
-        console.log("SOUNDCLOUD");
-
-        const comment = window.hypedditSettings.comment;
-
-        if(document.getElementById("sc_comment_text") !== null) {
-          document
-          .getElementById("sc_comment_text")
-          .setAttribute("value", comment);
-        }
-
-        if(document.getElementById("step_sc") !== null) {
-          document.getElementById("step_sc").querySelector("a").click();
-        }
-      };
-
-      window.handleInstagram = function() {
-        console.log("INSTA");
-        window.handleFollowOptions("instagram_status", "skipper_ig_next");
-      };
-
-      window.handleYoutube = function() {
-        console.log("YOUTUBE");
-        window.handleFollowOptions("youtube_status", "skipper_yt_next");
-      };
-      if(document.getElementById("step_sc") !== null) {
-        document.getElementById("step_sc").querySelector("a").click();
-      }
     }
-    ;
+  };
+  window.handleSoundCloud = function() {
+    console.log("SOUNDCLOUD");
 
-    window.handleInstagram = function() {
-      console.log("INSTA");
-      window.handleFollowOptions("instagram_status", "skipper_ig_next");
-    };
+    const comment = window.hypedditSettings.comment;
 
-    window.handleYoutube = function() {
-      console.log("YOUTUBE");
-      window.handleFollowOptions("youtube_status", "skipper_yt_next");
-    };
+    if(document.getElementById("sc_comment_text") !== null) {
+      document
+      .getElementById("sc_comment_text")
+      .setAttribute("value", comment);
+    }
+//        if(document.getElementById("step_sc") !== null) {
+//          document.getElementById("step_sc").querySelector("a").click();
+//        }
+    if(document.getElementById("step_sc") !== null) {
+      document.getElementById("step_sc").querySelector("a#login_to_sc").click();
+    }
+  };
+  window.handleInstagram = function() {
+    console.log("INSTA");
+    window.handleFollowOptions("instagram_status", "skipper_ig_next");
+  };
 
-    window.handleSpotify = function() {
-      console.log("SPOTIFY");
-      document.getElementById("step_sp").querySelector("a").click();
-    };
+  window.handleYoutube = function() {
+    console.log("YOUTUBE");
+    window.handleFollowOptions("youtube_status", "skipper_yt_next");
+  };
+  window.handleSpotify = function() {
+    console.log("SPOTIFY");
+    document.getElementById("step_sp").querySelector("a").click();
+  };
+  window.handleDownload = function() {
+    const email = window.hypedditSettings.email;
+    const name = window.hypedditSettings.name;
+    console.log("DOWNLOAD");
+    document.getElementById("gateDownloadButton").click();
 
-    window.handleDownload = function() {
-      console.log("DOWNLOAD");
-      document.getElementById("gateDownloadButton").click();
+    if(document.getElementById("email_name") !== null) {
+      document.getElementById("email_name").setAttribute("value", name);
+    }
 
-      if(window.hypedditSettings.auto_close) {
-        const timeout = window.hypedditSettings.auto_close_timeout_in_ms;
-        setTimeout(function() {
-          close();
-        }, timeout);
-      }
-    };
+    if(document.getElementById("email_address") !== null) {
+      document
+      .getElementById("email_address")
+      .setAttribute("value", email);
+      document.getElementById("email_address").value = email;
+    }
+    if(document.getElementById('download_email_step_hide_heading')) {
+      document.getElementById('gateDownloadButton').click();
+      return;
+    }
+    document.getElementById("email_to_downloads_next").click();
+  };
+  window.handleTikTok = function() {
+    console.log("TIKTOK");
+    window.handleFollowOptions("tiktok_status", "skipper_tk_next");
+  };
+  window.handleFacebook = function() {
+    console.log("FACEBOOK");
+    document.getElementById("fbCarouselSocialSection").click();
+  };
+  window.handleMultiPortal = function() {
+    document.getElementById("step_email").previousElementSibling.click();
+    window.handleEmail();
+  };
+  window.handleEmailSoundCloud = function() {
+    document.getElementById("step_email").previousElementSibling.click();
+    window.handleEmail();
+  };
+  window.handleSoundCloudYoutube = function() {
+    document.getElementById("step_yt").previousElementSibling.click();
+    window.handleYoutube();
+  };
+  window.handleMixcloud = function() {
+    console.log("Mixcloud");
+    document.getElementById("skipper_mc").click();
+  };
+  window.handleDonate = function() {
+    document.getElementById("step_dn").previousElementSibling.click();
+    document.getElementById("donation_next").click();
+  };
+  window.handleBandCamp = function() {
+    document.getElementById("skipper_bc").click();
+  };
 
-    window.handleDownload = function() {
-      console.log("DOWNLOAD");
-      downloadUnlimitedGate();
+  targetNode = document.getElementById("myCarousel");
+  const config = {attributes : true, childList : true, subtree : true};
+  let prevStepContent = null;
+  let stepContent;
+  const callback = (mutationList, observer) => {
 
-      if(window.hypedditSettings.auto_close) {
-        const timeout = window.hypedditSettings.auto_close_timeout_in_ms;
-        setTimeout(function() {
-          close();
-        }, timeout);
-      }
-    };
-
-
-    window.handleDownload = function() {
-      const email = window.hypedditSettings.email;
-      const name = window.hypedditSettings.name;
-      console.log("DOWNLOAD");
-      document.getElementById("gateDownloadButton").click();
-
-      if(document.getElementById("email_name") !== null) {
-        document.getElementById("email_name").setAttribute("value", name);
-      }
-
-      if(document.getElementById("email_address") !== null) {
-        document
-        .getElementById("email_address")
-        .setAttribute("value", email);
-        document.getElementById("email_address").value = email;
-      }
-      if(document.getElementById('download_email_step_hide_heading')) {
-        document.getElementById('gateDownloadButton').click();
-        return;
-      }
-      document.getElementById("email_to_downloads_next").click();
-    };
-
-    window.handleTikTok = function() {
-      console.log("TIKTOK");
-      window.handleFollowOptions("tiktok_status", "skipper_tk_next");
-    };
-
-    window.handleFacebook = function() {
-      console.log("FACEBOOK");
-      document.getElementById("fbCarouselSocialSection").click();
-    };
-
-    window.handleMultiPortal = function() {
-      document.getElementById("step_email").previousElementSibling.click();
-      window.handleEmail();
-    };
-
-    window.handleEmailSoundCloud = function() {
-      document.getElementById("step_email").previousElementSibling.click();
-      window.handleEmail();
-    };
-
-    window.handleSoundCloudYoutube = function() {
-      document.getElementById("step_yt").previousElementSibling.click();
-      window.handleYoutube();
-    };
-    window.handleMixcloud = function() {
-      console.log("Mixcloud");
-      document.getElementById("skipper_mc").click();
-    };
-
-
-    window.handleDonate = function() {
-      document.getElementById("step_dn").previousElementSibling.click();
-      document.getElementById("donation_next").click();
-    };
+    for(const mutation of mutationList) {
+      if(mutation.type === "attributes") {
+        const stepContent = document.querySelector(
+        ".fangate-slider-content:not(.move-left)"
+        );
 
 
-    targetNode = document.getElementById("myCarousel");
-    const config = {attributes : true, childList : true, subtree : true};
-    window.handleBandCamp = function() {
-      document.getElementById("skipper_bc").click();
-    };
+        if(stepContent !== prevStepContent) {
+          const stepClassList = stepContent.classList;
 
-
-    let prevStepContent = null;
-    const callback = (mutationList, observer) => {
-      for(const mutation of mutationList) {
-        if(mutation.type === "attributes") {
-          const stepContent = document.querySelector(
-          ".fangate-slider-content:not(.move-left)"
-          );
-
-
-          if(stepContent !== prevStepContent) {
-            const stepClassList = stepContent.classList;
-
-            if(stepClassList.contains("sp|ig|email")) {
-              window.handleMultiPortal();
-            }
-
-            if(stepClassList.contains("tk|ig")) {
-              window.handleTikTok();
-            }
-
-
-            if(stepClassList.contains("tk|ig")) {
-              window.handleTikTok();
-            }
-
-            if(stepClassList.contains("sp|ig|email")) {
-              window.handleMultiPortal();
-            }
-
-            if(stepClassList.contains("dn")) {
-              window.handleDonate();
-            }
-
-            if(stepClassList.contains("sc")) {
-              window.handleSoundCloud();
-            }
-
-            if(stepClassList.contains("ig")) {
-              window.handleInstagram();
-            }
-
-            if(stepClassList.contains("dw")) {
-              window.handleDownload();
-            }
-
-            if(stepClassList.contains("yt")) {
-              window.handleYoutube();
-            }
-
-            if(stepClassList.contains("sp")) {
-              window.handleSpotify();
-            }
-
-            if(stepClassList.contains("fb")) {
-              window.handleFacebook();
-            }
-
-            if(stepClassList.contains("mc")) {
-              window.handleMixcloud();
-            }
+          if(stepClassList.contains("sp|ig|email")) {
+            window.handleMultiPortal();
           }
 
+          if(stepClassList.contains("tk|ig")) {
+            window.handleTikTok();
+          }
+
+
+          if(stepClassList.contains("tk|ig")) {
+            window.handleTikTok();
+          }
+
+          if(stepClassList.contains("sp|ig|email")) {
+            window.handleMultiPortal();
+          }
+
+          if(stepClassList.contains("dn")) {
+            window.handleDonate();
+          }
+
+          if(stepClassList.contains("sc")) {
+            window.handleSoundCloud();
+          }
+
+          if(stepClassList.contains("ig")) {
+            window.handleInstagram();
+          }
+
+          if(stepClassList.contains("dw")) {
+            window.handleDownload();
+          }
+
+          if(stepClassList.contains("yt")) {
+            window.handleYoutube();
+          }
+
+          if(stepClassList.contains("sp")) {
+            window.handleSpotify();
+          }
+
+          if(stepClassList.contains("fb")) {
+            window.handleFacebook();
+          }
+
+          if(stepClassList.contains("mc")) {
+            window.handleMixcloud();
+          }
           if(stepClassList.contains("email")) {
             window.handleEmail();
           }
@@ -501,25 +451,17 @@
           if(stepClassList.contains("bc")) {
             window.handleBandCamp();
           }
+          if(stepClassList.contains("fb")) {
+            window.handleFacebook();
+          }
+          prevStepContent = stepContent;
         }
-
-
-        if(stepClassList.contains("fb")) {
-          window.handleFacebook();
-        }
+        //END steps here
       }
-
-
-      if(stepClassList.contains("bc")) {
-        window.handleBandCamp();
-      }
-    };
-
-
-    prevStepContent = stepContent;
+    }
+    // end function body
   };
 
-// Fix: TypeError: MutationObserver.observe: Argument 1 is not an object.
   if(targetNode) {
     const observer = new MutationObserver(callback);
 
@@ -533,6 +475,4 @@
   };
 
   setTimeout(_start, 800);
-})
-();
-
+})();
